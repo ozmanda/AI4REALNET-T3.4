bc783e6638f7c3464bbd505c62c6d0ea5a2e6289
ozmanda <usher.juliapatricia@gmail.com>
2025-02-04 15:49:14 +0100

    Removed multiple action heads

--- networks/MLP.py
+++ networks/MLP.py
@@ -1,5 +1,5 @@
 '''
-From IC3Net models.py, continuous case removed (flatland has discrete action space)
+From IC3Net models.py, continuous case removed (flatland has discrete action space) and only single actions considered.
 '''
 
 import torch
@@ -17,10 +17,7 @@ class MLP(nn.Module):
         self.args = args
         self.fc1 = nn.Linear(num_inputs, args.hid_size)
         self.fc2 = nn.Linear(args.hid_size, args.hid_size)
-
-        self.action_mean = nn.Linear(args.hid_size, args.dim_actions)
-        self.action_log_std = nn.Parameter(torch.zeros(1, args.dim_actions))
-
+        self.action_head = nn.Linear(args.hid_size, args.n_actions)
         self.value_head = nn.Linear(args.hid_size, 1)
 
 
@@ -28,9 +25,5 @@ class MLP(nn.Module):
         x = nn.Tanh(self.fc1(x))
         h = nn.Tanh(sum([self.fc2(x), x]))
         v = self.value_head(h)
-
-        action_mean = self.action_mean(h)
-        action_log_std = self.action_log_std.expand_as(action_mean)
-        action_std = torch.exp(action_log_std)
-        return (action_mean, action_log_std, action_std), v
+        return F.log_softmax(self.action_head(h), dim=-1)
         
