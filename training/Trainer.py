60204f217757ecedf9be92808579e55e5becd6a7
ozmanda <usher.juliapatricia@gmail.com>
2025-02-04 16:30:30 +0100

    Changed to base trainer for MLP networks

--- training/Trainer.py
+++ training/Trainer.py
@@ -1,40 +1,55 @@
 '''
-From IC3Net trainer.py 
+Adapted from IC3Net trainer.py to be a base trainer class, and trainer for MLP. The original code considers all network types during training. 
 '''
 
 import numpy as np
 import torch
 from torch import optim, Tensor
 import torch.nn as nn
-from typing import Tuple
+from typing import Tuple, List
+from argparse import Namespace
+from utils.action_utils import sample_action
 
 class Trainer():
-    def __init__(self, args, policy_net, env): 
-        self.args = args
+    def __init__(self, args: Namespace, policy_net, env): 
+        self.args: Namespace = args
         self.policy_net = policy_net
         self.env = env
+
+        self.stats: dict = dict()
 
         self.optimizer = optim.rmsprop.RMSprop(policy_net.parameters(), lr=args.lr, alpha=args.alpha, eps=args.eps)
         self.params = [p for p in self.policy_net.parameters()]
 
     
     def get_episode(self, epoch):
+        # TODO: keep only base case intialisation in this function
         episode = []
-        obs = self.env.reset()
-
+        observations = self.env.reset()
         state = dict()
-        info = dict()
 
-        # initialisations
-        if self.args.commnet:
-            info['comm_action'] = torch.zeros(self.n_agents, dytpe=int)
-        if self.args.rnn_type == 'LSTM':
-            prev_hid: Tuple[Tensor, Tensor] = self.policy_net.init_hidden(self.n_agents)
-        else:
-            prev_hid: Tensor = torch.zeros(1, self.n_agents, self.args.hid_size)
+        action: List[Tensor]
+        value: Tensor
+        prev_hidden_state: Tensor = torch.zeros(1, self.args.n_agents, self.args.hid_size)
 
-        for t in range(self.args.max_steps):
-            if self.recurrent: 
-                #! the original code combines tuples and tensors, keep them separated here for clarity
-                x = [state, prev_hid]
-                action_out, value, prev_hid = self.policy_net(x, info) #! this isn't adapted yet. 
+        while True: 
+            action_probs, value = self.policy_net(observations)
+            action = sample_action(action_probs)
+            action = [x.squeeze().data.numpy() for x in action] 
+            # translate into env action
+            # action = translate_action(self.args, self.env, action)
+
+
+            step += 1
+            if step >= self.args.max_steps:
+                break
+
+
+    
+    def get_batch(self) -> Tuple[List[Tensor], dict]:
+        batch: List[Tensor] = []
+        self.stats['num_episodes'] = 0
+        
+        while len(batch) < self.args.batch_size:
+            pass
+                       
