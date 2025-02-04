f651c923eeb64352b586410093cdcda5bb215132
ozmanda <usher.juliapatricia@gmail.com>
2025-02-04 15:53:54 +0100

    typing and description

--- networks/MLP.py
+++ networks/MLP.py
@@ -2,13 +2,11 @@
 From IC3Net models.py, continuous case removed (flatland has discrete action space) and only single actions considered.
 '''
 
-import torch
 from torch import Tensor
-import torch.autograd as autograd
-from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 from argparse import Namespace
+from typing import Tuple
 
 class MLP(nn.Module):
     def __init__(self, args: Namespace, num_inputs: int):
@@ -21,9 +19,10 @@ class MLP(nn.Module):
         self.value_head = nn.Linear(args.hid_size, 1)
 
 
-    def forward(self, x: Tensor, info={}):
+    def forward(self, x: Tensor) -> Tuple[Tensor, Tensor]:
+        ''' Outputs the log-probability of each action in a Tensor of length n_actions'''
         x = nn.Tanh(self.fc1(x))
         h = nn.Tanh(sum([self.fc2(x), x]))
         v = self.value_head(h)
-        return F.log_softmax(self.action_head(h), dim=-1)
+        return F.log_softmax(self.action_head(h), dim=-1), v
         
