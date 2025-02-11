81ae0db3a5ed397e438edafb2250e2b4806e4ea1
ozmanda <usher.juliapatricia@gmail.com>
2025-02-11 16:51:47 +0100

    typing and transition dataclass

--- training/Trainer.py
+++ training/Trainer.py
@@ -9,36 +9,50 @@ import torch.nn as nn
 from typing import Tuple, List
 from argparse import Namespace
 from utils.action_utils import sample_action
+from networks.MLP import MLP
+from flatland.envs.rail_env import RailEnv
+from collections import namedtuple
+from dataclasses import dataclass
+
+Transition = namedtuple('Transition', ('state', 'action', 'value', 'reward', 'done'))
+
+@dataclass
+class Transition:
+    state: dict
+    action: dict
+    value: float
+    reward: dict
+    done: dict
+
 
 class Trainer():
-    def __init__(self, args: Namespace, policy_net, env): 
+    def __init__(self, args: Namespace, policy_net: MLP, env: RailEnv): 
         self.args: Namespace = args
-        self.policy_net = policy_net
-        self.env = env
+        self.policy_net: MLP = policy_net
+        self.env: RailEnv = env
+        if self.obs_type == 'tree': 
+            self.max_tree_depth = args.max_tree_depth
 
         self.stats: dict = dict()
 
         self.optimizer = optim.rmsprop.RMSprop(policy_net.parameters(), lr=args.lr, alpha=args.alpha, eps=args.eps)
         self.params = [p for p in self.policy_net.parameters()]
 
-    
+
     def get_episode(self, epoch):
-        # TODO: keep only base case intialisation in this function
-        episode = []
-        observations = self.env.reset()
-        state = dict()
+        episode: List = []
+        observations: Tuple[dict, dict] = self.env.reset()
+        state: dict = dict()
 
         action: List[Tensor]
         value: Tensor
         prev_hidden_state: Tensor = torch.zeros(1, self.args.n_agents, self.args.hid_size)
 
+        step = 0
         while True: 
             action_probs, value = self.policy_net(observations)
-            action = sample_action(action_probs)
-            action = [x.squeeze().data.numpy() for x in action] 
-            # translate into env action
-            # action = translate_action(self.args, self.env, action)
-
+            action: Tensor = sample_action(action_probs)
+            next_state, reward, done, info = self.env.step(action)
 
             step += 1
             if step >= self.args.max_steps:
