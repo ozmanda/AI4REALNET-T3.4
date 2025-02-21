12b842d0b6230c29ba6dfcb2d98fc76dfa91a3ce
ozmanda <usher.juliapatricia@gmail.com>
2025-02-21 13:54:35 +0100

    changed value_head -> critic and actor_head -> critic

--- networks/MLP.py
+++ networks/MLP.py
@@ -11,18 +11,22 @@ from typing import Tuple
 class MLP(nn.Module):
     def __init__(self, args: Namespace, num_inputs: int) -> None:
         super().__init__()
+        """
+        Basic MLP network with fully connected layers and action / value heads. The action
+        and value heads are synonymous with actor and critic networks, respectively. 
+        """
 
         self.args = args
         self.fc1 = nn.Linear(num_inputs, args.hid_size)
         self.fc2 = nn.Linear(args.hid_size, args.hid_size)
-        self.action_head = nn.Linear(args.hid_size, args.n_actions)
-        self.value_head = nn.Linear(args.hid_size, 1)
+        self.actor = nn.Linear(args.hid_size, args.n_actions)
+        self.critic = nn.Linear(args.hid_size, 1)
 
 
     def forward(self, x: Tensor) -> Tuple[Tensor, Tensor]:
         ''' Outputs the log-probability of each action in a Tensor of length n_actions'''
         x = nn.Tanh(self.fc1(x))
         h = nn.Tanh(sum([self.fc2(x), x]))
-        v = self.value_head(h)
-        return F.log_softmax(self.action_head(h), dim=-1), v
+        v = self.critic(h)
+        return F.log_softmax(self.actor(h), dim=-1), v
         
