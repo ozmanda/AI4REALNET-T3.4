362d2de31b1afe780aa7f1990314ceedd1084331
ozmanda <usher.juliapatricia@gmail.com>
2025-02-24 09:32:00 +0100

    command line arguments and environment generation

--- main.py
+++ main.py
@@ -11,18 +11,31 @@ from flatland.envs.rail_env import RailEnv
 
 if __name__ == '__main_': 
     parser = ArgumentParser()
+
+    # Environment Parameters
+    parser.add_argument('--observation_type', type=str, help='Type of observation to use', default='tree')
+    parser.add_argument('--max_tree_depth', type=int, help='Maximum depth of the tree observation if tree observation is used', default=3)
+
+    # Network Parameters
     parser.add_argument('--comm', type=bool, help='Whether to use communication in the network', default=False)
     parser.add_argument('--hid_size', type=int, help='Size of the hidden layers', default=64)
     parser.add_argument('--recurrent', type=bool, help='Whether to use a recurrent network', default=True)
     parser.add_argument('--rnn_type', type=str, help='Type of RNN to use', default='lstm')
+
+    # Learning Parameters
     parser.add_argument('--detach_gap', type=int, help='Number of steps before detaching the hidden state', default=1)
-    parser.add_argument('--observation_type', type=str, help='Type of observation to use', default='tree')
-    parser.add_argument('--max_tree_depth', type=int, help='Maximum depth of the tree observation if tree observation is used', default=3)
+    parser.add_argument('--normalise_rewards', type=bool, help='Whether to normalise rewards', default=True)
+    parser.add_argument('--epsilon', type=float, help='Epsilon value for the epsilon greedy policy', default=0.2)
+    parser.add_argument('--value_coefficient', type=float, help='Coefficient for the value loss', default=1)
+    parser.add_argument('--entropy_coefficient', type=float, help='Coefficient for the entropy loss', default=0.001)
+    parser.add_argument('--normalise_advantage', type=bool, help='Whether to normalise the advantage', default=True)
+    parser.add_argument('--gamma', type=float, help='Discount factor for rewards', default=0.99)
 
     args: Namespace = parser.parse_args()
 
 
-    env: RailEnv = small_flatland_env()
+    env: RailEnv = small_flatland_env(observation=args.observation_type, max_tree_depth=args.max_tree_depth)
+    
     if args.observation_type == 'tree':
         tree_nodes = int((4 ** (args.max_tree_depth + 1) - 1) / 3)   # geometric progression
         obs_features = 12
