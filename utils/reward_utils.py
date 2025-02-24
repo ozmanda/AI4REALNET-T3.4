d72f536a67aa80eb6e1c362a07d6591af4d88789
ozmanda <usher.juliapatricia@gmail.com>
2025-02-24 09:39:43 +0100

    reward_utils initial commit

---
+++ utils/reward_utils.py
@@ -0,0 +1,28 @@
+import torch
+import numpy as np
+from torch import Tensor
+
+def compute_discounted_reward_per_agent(rewards: Tensor, gamma: float) -> Tensor: 
+    """
+    Computes the discounted reward / return / reward-to-go for each agent. 
+
+    Input:
+        - rewards           Tensor (batch_size, n_agents)      [float] 
+
+    Output:
+        - discounted_reward Tensor (batch_size, n_agents)       [float] 
+    """
+    batch_size = rewards.size(0)
+    n_agents = rewards.size(1)
+    future_reward = torch.zeros(n_agents)
+    discounted_rewards = torch.zeros(batch_size, n_agents)
+    advantages = torch.zeros(batch_size, n_agents)
+
+    for sample in reversed(range(batch_size)):
+        returns = rewards[sample] + gamma * future_reward # (n_agents)
+        discounted_rewards[sample] = returns.clone()
+        future_reward = returns.clone()
+
+    return discounted_rewards
+
+
