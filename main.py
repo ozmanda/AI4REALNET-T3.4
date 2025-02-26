b3974c3ac3f9a4d3b7d47c3ea1ad4d02f8ea574a
ozmanda <usher.juliapatricia@gmail.com>
2025-02-26 15:20:55 +0100

    added model saving, loading and training logs

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ from networks.MLP import MLP
 from environments.env_small import small_flatland_env
 from flatland.envs.rail_env import RailEnv
 import time
+import torch
+import numpy as np
 from utils.utils import merge_dicts
+import os
+from utils.log_utils import init_logger
+from typing import Dict, NamedTuple
 
 
 if __name__ == '__main__': 
@@ -19,6 +24,21 @@ if __name__ == '__main__':
     parser.add_argument('--epoch_size', type=int, help='Number of steps per epoch', default=10)
     parser.add_argument('--batch_size', type=int, help='Batch size for training', default=500)
     parser.add_argument('--max_steps', type=int, help='Maximum number of steps per episode', default=1000)
+
+    # Model Saving
+    parser.add_argument('--save', type=bool, help='Whether to save the model', default=True)
+    parser.add_argument('--save_whole_model', type=bool, help='Whether to save the whole model or just the state dict', default=False)
+    parser.add_argument('--save_dir', type=str, help='Directory to save models', default='models/')
+    parser.add_argument('--save_every', type=int, help='Frequency of saving models', default=10)
+    parser.add_argument('--model_name', type=str, help='Name of the model to save without an extension', default=None)
+
+    # Model Loading
+    parser.add_argument('--load', type=str, help='Model to load', default=False)
+    parser.add_argument('--load_dir', type=str, help='Directory to load models from', default='models/')
+
+    # Log Settings
+    parser.add_argument('--log', type=bool, help='Whether to log the training', default=True)
+    parser.add_argument('--log_dir', type=str, help='Directory to save logs, default is modeldir', default=None)
 
     # Environment Parameters
     parser.add_argument('--observation_type', type=str, help='Type of observation to use', default='tree')
@@ -45,12 +65,13 @@ if __name__ == '__main__':
 
     args: Namespace = parser.parse_args()
 
-
+    # Environment initialisation 
     env: RailEnv = small_flatland_env(observation=args.observation_type, max_tree_depth=args.max_tree_depth)
     _ = env.reset()
     args.n_agents = env.get_num_agents()
     args.n_actions = env.action_space[0]
 
+    # Observation settings
     if args.observation_type == 'tree':
         tree_nodes = int((4 ** (args.max_tree_depth + 1) - 1) / 3)   # geometric progression
         obs_features = 12
@@ -59,8 +80,20 @@ if __name__ == '__main__':
         obs_inputs = env.observation_space[0].shape[0] * env.observation_space[0].shape[1] * 23
     else: 
         raise ValueError('Invalid observation type')
+    
+    # model saving settings
+    if args.save:
+        if not args.model_name:
+            args.model_name = f'model_{time.strftime("%d%m%y_%H%M")}'
+        args.save_dir = os.path.join(args.save_dir, args.model_name)
 
+    # logger initialisation
+    if args.log:
+        log: Dict[str, NamedTuple] = init_logger()
+        if not args.log_dir:
+            args.log_dir = args.save_dir
 
+    # Network initialisation
     if args.recurrent:
         policynet = LSTM(args, obs_inputs) if args.rnn_type == 'lstm' else RNN(args, obs_inputs)
         trainer = RNNTrainer(args, policynet, env)
@@ -70,9 +103,21 @@ if __name__ == '__main__':
     else: 
         policynet = MLP(args, obs_inputs)
         trainer = Trainer(args, policynet, env)
+    
+    # load most recent saved model
+    if args.load:
+        model_files = [f for f in os.listdir(args.load_dir) if os.path.isfile(os.path.join(args.load_dir, f))]
+        if model_files:
+            latest_model = max(model_files, key=lambda x: os.path.getctime(os.path.join(args.load_dir, x)))
+            args.load = os.path.join(args.load_dir, latest_model)
+        else:
+            raise FileNotFoundError(f"No model files found in directory {args.load_dir}")
+        
+        policynet.load_state_dict(torch.load(args.load))
 
+    #? move this into the trainer class?
+    # Main epoch training loop
     for epoch in range(args.epochs):
-
         epoch_start_time = time.time()
         epoch_stats = dict()
 
@@ -80,5 +125,9 @@ if __name__ == '__main__':
             batch_info = trainer.train_batch()
 
         epoch_time = time.time() - epoch_start_time
-        print(epoch_time)
+        print(f'Epoch {epoch} \tReward {np.sum(batch_info["reward"])} \tTime {epoch_time}')
+
+        if args.save:
+            if epoch % args.save_every == 0: 
+                trainer.save_model(f'{args.save_dir}model_state_{epoch}')
         
