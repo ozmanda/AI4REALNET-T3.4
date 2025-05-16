21966b314cb9d1092845713252a50f7c719825b4
ozmanda <usher.juliapatricia@gmail.com>
2025-05-16 14:43:19 +0200

    bugfix

--- src/configs/ControllerConfigs.py
+++ src/configs/ControllerConfigs.py
@@ -5,8 +5,8 @@ class PPOControllerConfig():
     def __init__(self, config_dict: Dict[str, Union[Dict, str, float, int]], device: str) -> None:
         self.device = device
         self.config_dict = config_dict
-        self.config_dict['actor_config']['actor_layers_sizes'] = [int(x) for x in config_dict['actor_layers_sizes'].values()]
-        self.config_dict['critic_config']['critic_layers_sizes'] = [int(x) for x in config_dict['critic_layers_sizes'].values()]
+        self.config_dict['actor_config']['actor_layers_sizes'] = [int(x) for x in config_dict['actor_config']['actor_layers_sizes'].values()]
+        self.config_dict['critic_config']['critic_layers_sizes'] = [int(x) for x in config_dict['critic_config']['critic_layers_sizes'].values()]
 
 
     def create_controller(self) -> PPOController:
