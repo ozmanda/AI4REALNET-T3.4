c36c9614d017eb005b6cecfaa864043b2391f90d
ozmanda <usher.juliapatricia@gmail.com>
2025-05-26 17:57:49 +0200

    dictionary keys changed

--- src/configs/ControllerConfigs.py
+++ src/configs/ControllerConfigs.py
@@ -1,12 +1,13 @@
 from src.algorithms.PPO.PPOController import PPOController
 from typing import Dict, List, Union
+from src.utils.obs_utils import calculate_state_size
 
 class PPOControllerConfig(): 
     def __init__(self, config_dict: Dict[str, Union[Dict, str, float, int]], device: str) -> None:
         self.device = device
         self.config_dict = config_dict
-        self.config_dict['actor_config']['actor_layer_sizes'] = [int(x) for x in config_dict['actor_config']['actor_layer_sizes']]
-        self.config_dict['critic_config']['critic_layer_sizes'] = [int(x) for x in config_dict['critic_config']['critic_layer_sizes'].values()]
+        self.config_dict['actor_config']['layer_sizes'] = [int(x) for x in config_dict['actor_config']['layer_sizes']]
+        self.config_dict['critic_config']['layer_sizes'] = [int(x) for x in config_dict['critic_config']['layer_sizes']]
 
 
     def create_controller(self) -> PPOController:
