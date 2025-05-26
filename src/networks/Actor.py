30493a5191732fefde7424c675c012ef52c7b53e
ozmanda <usher.juliapatricia@gmail.com>
2025-05-26 17:58:20 +0200

    adjustments to layers and bugfixing

--- src/networks/Actor.py
+++ src/networks/Actor.py
@@ -18,36 +18,37 @@ from src.utils.obs_utils import direction_tensor
 
 
 class Actor(nn.Module):  
-    def __init__(self, config_dict: Dict, n_features: int, n_actions: int):
+    def __init__(self, config_dict: Dict, n_features: int, n_actions: int): # TODO: change config_dict to DefaultDict, add recursive layer hidden size parameter
         super().__init__()
-        self.n_actions = n_actions
+        self.n_actions = n_actions # TODO: check that this comes from env.
         self.n_features = n_features
+        self.input_size = n_features * config_dict['n_nodes']
         self.hid_size: int = config_dict['hidden_size']
         self.layer_sizes: List[int] = config_dict['layer_sizes']
         self.intent_size: int = config_dict['intent_size']
         self.neighbour_depth: int = config_dict['neighbour_depth']
         
-        self.state_encoder = nn.Sequential(RecursiveLayer(n_features, self.hid_size))
-        self.policy_hidden_layer = nn.Linear(self.hid_size, self.intent_size + self.neighbour_depth)
+        self.state_encoder = nn.Sequential(RecursiveLayer(n_features=n_features, n_nodes=config_dict['n_nodes'], output_size=self.hid_size))
+        self.policy_hidden_layer = nn.Linear(self.hid_size, self.intent_size)
         self.intent_encoding = nn.Sequential(nn.Linear(self.hid_size, self.intent_size, bias=False),
                                                   nn.ReLU(inplace=True))
-        self.intent_attention = MultiHeadAttention(n_features=n_features, num_heads=config_dict['n_heads'])
-        self.action_decoder = nn.Sequential(nn.Linear(config_dict['thought_size'] + (config_dict['intent_size'] + self.neighbour_depth) * config_dict['n_heads'], 256), 
-                                            nn.ReLU(inplace=True), 
+        self.intent_attention = MultiHeadAttention(hid_size=self.hid_size, intent_size=self.intent_size, num_heads=config_dict['n_heads'])
+        self.action_decoder = nn.Sequential(nn.Linear((self.intent_size * config_dict['n_heads']) + self.hid_size, 256),
+                                            nn.ReLU(inplace=True),
                                             nn.Linear(256, self.n_actions))
 
         self.direction_tensor: Tensor = direction_tensor(self.neighbour_depth)
         
     
-    def encode(self, state: Tensor) -> Tensor:
-        return self.state_encoder(state)
+    def encode(self, states: Tensor) -> Tensor:
+        return self.state_encoder(states)
 
 
     def intent(self, encoded_state: Tensor) -> Tensor:
         return self.intent_encoding(encoded_state)
 
 
-    def act(self, encoded_state: Tensor, agent_signals: Tensor) -> Tensor:
+    def act(self, encoded_states: Tensor, agent_signals: Tensor) -> Tensor:
         """
         Outputs the log_probabilities of the action options
 
@@ -55,11 +56,13 @@ class Actor(nn.Module):
             - encoded_state     Tensor      (batch_size, hidden_size)
             - agent_signals     Tensor      (batch_size, n_agents, intention_size)
         """
-        query: Tensor = self.policy_hidden_layer(encoded_state).unsqueeze(-1)
-        key: Tensor = self._add_direction(agent_signals)
+        # resize to 
+
+        query: Tensor = self.policy_hidden_layer(encoded_states).unsqueeze(1)
+        # key: Tensor = self._add_direction(agent_signals) #! removed direction information
+        key = agent_signals.view(agent_signals.size(0), agent_signals.size(1), -1)
         attended_signals: Tensor = self.intent_attention(query=query, key=key, value=key)
-        attended_signals = attended_signals.squeeze(1)
-        input = torch.cat([encoded_state, attended_signals], dim=1)
+        input = torch.cat([encoded_states, attended_signals], dim=1)
         return self.action_decoder(input)
 
 
