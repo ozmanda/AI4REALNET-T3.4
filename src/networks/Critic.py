edbf2378b4ec8495a95e2487c05228a2bb9f5a14
ozmanda <usher.juliapatricia@gmail.com>
2025-05-26 17:58:55 +0200

    bugfixes

--- src/networks/Critic.py
+++ src/networks/Critic.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from torch import Tensor
-from typing import List
+from typing import List, Dict
 from argparse import Namespace
 
 from src.networks.Attention import MultiHeadAttention
@@ -11,10 +11,11 @@ from src.networks.Recursive import RecursiveLayer
 from src.utils.network_utils import create_linear_layers
 
 class Critic(nn.Module): 
-    def __init__(self, n_features: int, n_actions: int, layer_sizes: List[int]): 
+    def __init__(self, n_features: int, config: Dict): 
         super().__init__()
-        layers: List[nn.Linear] = create_linear_layers(input_size=n_features, output_size=1, layer_sizes=layer_sizes) #? where are these used?
-        self.seq = nn.Sequential(RecursiveLayer(n_features, 1))
+        input_size = n_features * config['n_nodes']
+        layers: List[nn.Linear] = create_linear_layers(input_size=input_size, output_size=1, layer_sizes=config['layer_sizes']) # TODO: where are these used?
+        self.seq = nn.Sequential(RecursiveLayer(n_features=n_features, output_size=1, n_nodes=config['n_nodes']),)
 
     def forward(self, state: Tensor):
         return self.seq(state)
