7d8585f9aaa2da2d4e2b07b07d50e14cb0fdc4a9
ozmanda <usher.juliapatricia@gmail.com>
2025-08-07 15:40:43 +0200

    added _make_logits function

--- src/algorithms/PPO/PPOController.py
+++ src/algorithms/PPO/PPOController.py
@@ -2,18 +2,15 @@ import wandb
 import torch
 import torch.nn as nn
 from torch import Tensor
-import torch.optim as optim
 import torch.nn.functional as F
 import numpy as np
 from typing import Dict, Union, List, Tuple
-from itertools import chain
 
 from flatland.envs.rail_env import RailEnv
 
 from src.memory.MultiAgentRolloutBuffer import MultiAgentRolloutBuffer
 from src.networks.FeedForwardNN import FeedForwardNN
 
-from src.training.loss import value_loss, value_loss_with_IS, policy_loss
 
 class PPOController():
     def __init__(self, config: Dict, agent_ID: Union[int, str] = None):
@@ -24,7 +21,6 @@ class PPOController():
 
         self.actor: nn.Module = self._build_actor()
         self.critic: nn.Module = self._build_critic()
-        self.optimizer: optim.Optimizer = self._build_optimizer(config['optimiser_config'])
         self.update_step: int = 0
 
     def _init_hyperparameters(self, config: Dict) -> None:
@@ -47,77 +43,17 @@ class PPOController():
     def _build_critic(self) -> nn.Module:
         self.critic_network = FeedForwardNN(self.state_size, self.config['critic_config']['hidden_size'], 1) # TODO: add hidden size to critic config
 
-    def _build_optimizer(self, optimiser_config: Dict[str, Union[int, str]]) -> optim.Optimizer:
-        if optimiser_config['type'] == 'adam': 
-            self.optimiser = optim.Adam(params=chain(self.actor_network.parameters(), self.critic_network.parameters()), lr=float(optimiser_config['learning_rate']))
-        else: 
-            raise Warning('Only Adam optimiser has been implemented')
-
-
-    def update_networks(self, rollout: MultiAgentRolloutBuffer, epochs: int, batch_size: int, IS: bool = False) -> None:
+    def _make_logits(self, states: Tensor) -> Tensor:
         """
-        Update the actor and critic networks using the collected transitions from the rollout buffer.
-        1. for each agent and episode, compute the GAE
-        3. compute the loss
-        4. update the actor and critic networks
+        Create logits for the action space based on the current state.
+        
+        Parameters:
+            - states: Tensor (batch_size, n_features)
+        
+        Returns:
+            - logits: Tensor (batch_size, n_actions)
         """
-        rollout = self._generalised_advantage_estimation(rollout)
-        losses = {
-            'policy_loss': [],
-            'value_loss': []
-        }
-
-        for minibatch in rollout.get_minibatches(batch_size=batch_size, shuffle=False):
-            # forward pass through the actor network to get actions and log probabilities
-            new_log_probs, entropy, new_state_values, new_next_state_values = self._evaluate(minibatch['states'], minibatch['next_states'], minibatch['actions'])
-            new_state_values = new_state_values.squeeze(-1)  
-            new_next_state_values = new_next_state_values.squeeze(-1)
-
-            # Policy loss
-            old_log_probs = minibatch['log_probs']
-            actor_loss = policy_loss(gae=minibatch['gaes'],
-                                    new_log_prob=new_log_probs,
-                                    old_log_prob=old_log_probs,
-                                    clip_eps=self.clip_epsilon)
-            
-            # Value loss
-            if IS:
-                critic_loss = value_loss_with_IS(state_values=new_state_values,
-                                                next_state_values=new_next_state_values,
-                                                new_log_prob=new_log_probs,
-                                                old_log_prob=old_log_probs,
-                                                reward=minibatch['rewards'],
-                                                done=minibatch['dones'],
-                                                gamma=self.gamma
-                                                )
-            else:
-                critic_loss = value_loss(state_values=new_state_values,
-                                        next_state_values=new_next_state_values,
-                                        reward=minibatch['rewards'],
-                                        done=minibatch['dones'],
-                                        gamma=self.gamma)
-                
-            # Entropy bonus
-            entropy_loss = -entropy.mean()  # Encourage exploration
-
-            # Total loss & optimisation step
-            total_loss = actor_loss + critic_loss * self.value_loss_coef + entropy_loss * self.entropy_coef
-            self.optimiser.zero_grad()
-            total_loss.backward()
-            self.optimiser.step()
-            self.update_step += 1
-
-            # add metrics
-            losses['policy_loss'].append(actor_loss.mean().item())
-            losses['value_loss'].append(critic_loss.mean().item())
-            wandb.log({
-                'update_step': self.update_step,
-                'train/policy_loss': np.mean(losses['policy_loss']),
-                'train/value_loss': np.mean(losses['value_loss'])
-            })
-
-        return losses
-
+        return self.actor_network(states)
 
     def state_values(self, states: Tensor, next_states: Tensor) -> Tensor:
         """
@@ -147,7 +83,7 @@ class PPOController():
             - action: Tensor of shape (batch_size, action_size)
             - log_prob: Tensor of shape (batch_size, action_size)
         """
-        logits = self.actor_network(state)
+        logits = self._make_logits(state)
         action_distribution = torch.distributions.Categorical(logits=logits)
         actions = action_distribution.sample()
         log_prob = action_distribution.log_prob(actions)
@@ -166,42 +102,11 @@ class PPOController():
             - log_prob: Tensor of shape (batch_size, action_size)
         """
         with torch.no_grad():
-            logits = self.actor_network(state)
+            logits = self._make_logits(state)
             actions = torch.argmax(logits, dim=-1)
             log_probs = torch.log_softmax(logits, dim=1)
         return actions, log_probs
-        
-
-    def _generalised_advantage_estimation(self, rollout: MultiAgentRolloutBuffer, k: int = 1) -> Tensor:
-        """ 
-        Compute advantages using Generalized Advantage Estimation (GAE). 
-        The parameter k allows for a k-step TD error calculation, where k=1 corresponds to the standard TD(0) method.
-        The parameter gae_horizon truncates the GAE calculation to a fixed horizon, which can be useful for stabilizing training.
-        """
-        with torch.no_grad():
-            for idx, episode in enumerate(rollout.episodes):
-                for agent in rollout.agent_handles:
-                    states = torch.stack(episode['states'][agent])
-                    next_states = torch.stack(episode['next_states'][agent])
-                    rewards = torch.tensor(episode['rewards'][agent])
-                    dones = torch.tensor(episode['dones'][agent]).float()
-                    dones = (dones != 0).float()
-                    gaes = torch.zeros(len(states))
-
-                    state_values: Tensor = self.critic_network(states).squeeze(-1)
-                    next_state_values: Tensor = self.critic_network(next_states).squeeze(-1)
-
-                    rollout.episodes[idx]['state_values'][agent] = state_values
-                    rollout.episodes[idx]['next_state_values'][agent] = next_state_values
-
-                    deltas = rewards + self.gamma * next_state_values * (1 - dones) - state_values
-
-                    for i in reversed(range(len(deltas))):
-                        gaes[i] = deltas[i] + self.gamma * gaes[i + 1] * (1 - dones[i]) if i < len(deltas) - 2 else deltas[i]
-
-                    rollout.episodes[idx]['gaes'][agent] = gaes
-
-        return rollout
+    
 
     def _discounted_rewards(self, rewards: Tensor, dones: Tensor, step: int) -> Tensor:
         """ Compute discounted rewards. """
@@ -213,14 +118,4 @@ class PPOController():
         return discounted_rewards
     
 
-    def _evaluate(self, states: Tensor, next_states: Tensor, actions: Tensor) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
-        """ 
-        Forward pass through the actor network to get the action distribution and log probabilities, to compute the entropy of the policy distribution and the current state values.
-        """
-        logits = self.actor_network(states)
-        action_distribution = torch.distributions.Categorical(logits=logits)
-        log_probs = action_distribution.log_prob(actions)
-        entropy = action_distribution.entropy()
-        state_values = self.critic_network(states)
-        next_state_values = self.critic_network(next_states)
-        return log_probs, entropy, state_values, next_state_values
+
