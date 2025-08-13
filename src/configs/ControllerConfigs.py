e23d7973114748bfee2e80e4e19f9c0dcb0d4b97
ozmanda <usher.juliapatricia@gmail.com>
2025-08-13 16:53:56 +0200

    removed device

--- src/configs/ControllerConfigs.py
+++ src/configs/ControllerConfigs.py
@@ -3,12 +3,11 @@ from typing import Dict, List, Union
 from src.utils.obs_utils import calculate_state_size
 
 class PPOControllerConfig(): 
-    def __init__(self, config_dict: Dict[str, Union[Dict, str, float, int]], device: str) -> None:
-        self.device = device
+    def __init__(self, config_dict: Dict[str, Union[Dict, str, float, int]]) -> None:
         self.config_dict = config_dict
         self.config_dict['actor_config']['layer_sizes'] = [int(x) for x in config_dict['actor_config']['layer_sizes']]
         self.config_dict['critic_config']['layer_sizes'] = [int(x) for x in config_dict['critic_config']['layer_sizes']]
 
 
     def create_controller(self) -> PPOController:
-        return PPOController(self.config_dict, self.device) 
+        return PPOController(self.config_dict) 
