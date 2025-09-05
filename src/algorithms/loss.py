d0108aaec4ed2a91bc89dcbb842abe5a38429a53
ozmanda <usher.juliapatricia@gmail.com>
2025-09-05 09:16:38 +0200

    IS loss bugfix

--- src/algorithms/loss.py
+++ src/algorithms/loss.py
@@ -17,7 +17,7 @@ def value_loss_with_IS(state_values: Tensor, next_state_values: Tensor, new_log_
      -> adds importance sampling weights to account for policy changes, stabilising training by reducing the influence of value predictions based on large, potentially unreliable updates to the policy (=stabilisation)
     actual_len indicates the number of steps between state and next_state, default is 1.
     '''
-    expected_state_values = (next_state_values.detach() * torch.pow(gamma, actual_len) * (1 - done)) + reward
+    expected_state_values = (next_state_values.detach() * (gamma ** actual_len) * (1 - done)) + reward
     with torch.no_grad():
         truncated_ratio_log = torch.clamp(new_log_prob - old_log_prob, max=0)
         importance_sample_fix = torch.exp(truncated_ratio_log)
