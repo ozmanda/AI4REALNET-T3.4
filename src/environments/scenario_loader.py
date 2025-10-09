536be408b040ef19db6eababefab368151efb13a
ozmanda <usher.juliapatricia@gmail.com>
2025-10-09 07:47:18 +0200

    adjusted to flatland version 4.2.2

--- src/environments/scenario_loader.py
+++ src/environments/scenario_loader.py
@@ -6,6 +6,8 @@ from flatland.envs.rail_env import RailEnv
 from flatland.envs.timetable_utils import Line, Timetable
 from flatland.core.transition_map import GridTransitionMap
 from flatland.envs.grid.rail_env_grid import RailEnvTransitions
+from flatland.envs.rail_trainrun_data_structures import Waypoint
+from flatland.envs.observations import GlobalObsForRailEnv
 
 
 def rail_generator_from_grid_map(grid_map, level_free_positions):
@@ -31,7 +33,7 @@ def timetable_generator_from_timetable(timetable):
     return timetable_generator
 
 
-def load_scenario_from_json(scenario_path: str) -> RailEnv:
+def load_scenario_from_json(scenario_path: str, observation_builder=GlobalObsForRailEnv()) -> RailEnv:
     with open(scenario_path, 'r') as f:
         data = json.load(f)
 
@@ -40,9 +42,7 @@ def load_scenario_from_json(scenario_path: str) -> RailEnv:
     number_of_agents = len(data['flatland line']['agent_positions'])
 
     line = Line(
-        agent_positions = [[tuple(coords) for coords in positions] for positions in data['flatland line']['agent_positions']],
-        agent_directions = data['flatland line']['agent_directions'],
-        agent_targets = [tuple(coords) for coords in data['flatland line']['agent_targets']],
+        agent_waypoints=get_waypoints(data['flatland line']['agent_positions'], data['flatland line']['agent_targets'], data['flatland line']['agent_directions']),
         agent_speeds = data['flatland line']['agent_speeds'],
     )
 
@@ -56,8 +56,7 @@ def load_scenario_from_json(scenario_path: str) -> RailEnv:
     grid.grid = np.array(data['grid'])
 
     level_free_positions = [tuple(item) for item in data['overpasses']]
-
-
+    
     env = RailEnv(
         width = width,
         height = height,
@@ -65,8 +64,25 @@ def load_scenario_from_json(scenario_path: str) -> RailEnv:
         rail_generator = rail_generator_from_grid_map(grid, level_free_positions),
         line_generator = line_generator_from_line(line),
         timetable_generator = timetable_generator_from_timetable(timetable),
+        obs_builder_object = observation_builder,
     )
 
     env.stations = data['stations']
 
     return env
+
+
+def get_waypoints(agent_positions, agent_targets, agent_directions):
+    agent_waypoints = {i: [] for i in range(len(agent_positions))}
+    for agent, wpts in enumerate(agent_positions):
+        for idx, wpt in enumerate(wpts):
+            agent_waypoints[agent].append([Waypoint(position=tuple(wpt), direction=agent_directions[agent][idx])])
+    for agent, wpt in enumerate(agent_targets):
+        agent_waypoints[agent].append([Waypoint(position=tuple(wpt), direction=agent_directions[agent][-1])])
+    return agent_waypoints
+
+
+def get_num_agents(scenario_path: str) -> int:
+    with open(scenario_path, 'r') as f:
+        data = json.load(f)
+    return len(data['flatland line']['agent_positions'])
