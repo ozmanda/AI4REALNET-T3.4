420f7210cdc4f5b6d2152ff631116194081376de
ozmanda <usher.juliapatricia@gmail.com>
2025-10-21 12:47:58 +0200

    variable renaming

--- src/algorithms/loss.py
+++ src/algorithms/loss.py
@@ -3,26 +3,26 @@ from torch import Tensor
 import torch.nn.functional as F
 from typing import Tuple
 
-def value_loss(state_values: Tensor, next_state_values: Tensor, reward: Tensor, done: Tensor, gamma: float, actual_len: int = 1) -> Tensor:
+def value_loss(state_values: Tensor, new_state_values: Tensor, reward: Tensor, done: Tensor, gamma: float, actual_len: int = 1) -> Tensor:
     """
     Calculate the value loss for the critic network. The actual_len parameter is used to indicate the number of steps between state and next_state. Default is 1. 
     """
-    expected_state_values = (next_state_values * gamma ** actual_len * (1 - done)) + reward
+    expected_state_values = (new_state_values.detach() * gamma ** actual_len * (1 - done)) + reward
     return F.mse_loss(state_values, expected_state_values)
 
 
-def value_loss_with_IS(state_values: Tensor, next_state_values: Tensor, new_log_prob: Tensor, old_log_prob: Tensor, reward: Tensor, done: Tensor, gamma: float, actual_len: int = 1):
-    ''' 
+def value_loss_with_IS(state_values: Tensor, new_state_values: Tensor, new_log_prob: Tensor, old_log_prob: Tensor, reward: Tensor, done: Tensor, gamma: float, actual_len: int = 1):
+    '''
     Value Loss with Importance Sampling
      -> adds importance sampling weights to account for policy changes, stabilising training by reducing the influence of value predictions based on large, potentially unreliable updates to the policy (=stabilisation)
     actual_len indicates the number of steps between state and next_state, default is 1.
     '''
-    expected_state_values = (next_state_values * (gamma ** actual_len) * (1 - done)) + reward
+    expected_state_values = (new_state_values.detach() * (gamma ** actual_len) * (1 - done)) + reward
     with torch.no_grad():
         truncated_ratio_log = torch.clamp(new_log_prob - old_log_prob, max=0)
         importance_sample_fix = torch.exp(truncated_ratio_log)
     
-    value_loss = (F.mse_loss(expected_state_values, state_values, reduction="none") * importance_sample_fix).mean()
+    value_loss = (F.mse_loss(state_values, expected_state_values, reduction="none") * importance_sample_fix).mean()
     return value_loss
 
 
